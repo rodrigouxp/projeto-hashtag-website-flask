2b4a1fbc9a0f4b0203141dd1b9b4b50f53c6d722
Rodrigo Formigon <rodris.hud@gmail.com>
2022-05-26 17:02:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,15 @@
 # Flask v2.1.2
 from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/contato')
+def conato():
+    return 'Qualquer dúvida mande um e-mail para lalalala@gmail.com'
+
+if __name__ == '__main__':
+    app.run(debug=True)
