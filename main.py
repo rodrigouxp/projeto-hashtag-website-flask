fa38d2a9b72ba4923ae63d6fdd25a7a1103c7417
Rodrigo Formigon <rodris.hud@gmail.com>
2022-06-01 09:26:04 -0300

    Updates with html and new functions
    Advancing with the Flask framework functions

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # Python v3.10.4
 # Flask v2.1.2
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html')
 
 @app.route('/contato')
 def conato():
-    return 'Qualquer dúvida mande um e-mail para lalalala@gmail.com'
+    return render_template('contato.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
