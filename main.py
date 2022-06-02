6c7d5b18094d29ad3eba167ad6ef292a5aacdb72
Rodrigo Formigon <rodris.hud@gmail.com>
2022-06-02 15:59:14 -0300

    Update main and html modules
    Started to use bootstrap

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 # Python v3.10.4
 # Flask v2.1.2
-from flask import Flask, render_template
+# Bootstrap v5.2.x
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+lista_usuarios = ['Lira', 'João', 'Alan', 'Alessandra', 'Amanda']
+
+
 @app.route('/')
-def hello_world():
+def home():
     return render_template('home.html')
 
 @app.route('/contato')
-def conato():
+def contato():
     return render_template('contato.html')
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
