15fcdc9e8aa0b380b17828be8efccd527ca565a1
Rodrigo Formigon <rodris.hud@gmail.com>
2022-06-09 09:01:05 -0300

    Create better forms with error messages

--- forms.py
+++ forms.py
@@ -4,8 +4,8 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class FormLogin(FlaskForm):
-    email = StringField('E-mail')
-    password = PasswordField('Senha')
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
     lembrar_dados = BooleanField('Lembrar Dados de Acesso')
     button_submit_login = SubmitField('Login')
 
--- main.py
+++ main.py
@@ -28,11 +28,11 @@ def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
     
-    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+    if form_login.validate_on_submit() and 'button_submit_login' in request.form:
         flash(f'Login feito com sucesso no e-mail {form_login.email.data}', 'alert-success')
         return redirect(url_for('home'))
     
-    if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+    if form_criarconta.validate_on_submit() and 'button_submit_criarconta' in request.form:
         flash(f'Conta criada para o e-mail: {form_criarconta.email.data}', 'alert-success')
         return redirect(url_for('home'))
     
